80b913cf7d630268ed74e60540b4c0adb94b5181
21stFret <79184648+21stFret@users.noreply.github.com>
2023-09-12 23:24:06 +0100

    added gitignore

--- Unity/BOF3-Demo/Assets/Scripts/DialogueScriptable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/DialogueScript", order = 1)]
-public class DialogueScript : ScriptableObject
-{
-    public List<string> dialogueText;
-}
